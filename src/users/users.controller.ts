0bd3aaead6573e6522bb08390ba6a2fbc83230fd
ihorkanyewest <ihor@mirato.com>
2024-02-08 12:57:15 +0200

    upd

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dtos/create-user.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private userService: UsersService) {}
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {}
+  async createUser(@Body() body: CreateUserDto) {
+    await this.userService.create(body);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from 'src/users/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(props: Omit<User, 'id'>) {
+    const user = this.repo.create(props);
+
+    return this.repo.save(user);
+  }
+}
