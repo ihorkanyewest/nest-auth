10e08895e57da7a8e2fdfe5f6899fed3a906567b
ihorkanyewest <ihor@mirato.com>
2024-02-12 21:39:29 +0200

    fix tests

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -4,9 +4,17 @@ import { UsersService } from 'src/users/users.service';
 describe('UsersService', () => {
   let service: UsersService;
 
+  const fakeService = {
+    find: () => Promise.resolve([]),
+    create: () => Promise.resolve({}),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: UsersService, useValue: fakeService },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
