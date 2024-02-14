41d341e6680a77f40e04b1d4dd7b97ff7f0b0272
ihorkanyewest <ihor@mirato.com>
2024-02-14 18:10:43 +0200

    upd reports

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from 'src/reports/dtos/create-report.dto';
+import { ReportsService } from 'src/reports/reports.service';
+import { AuthGuard } from 'src/guards/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportService: ReportsService) {}
+  @Post()
+  async createReport(@Body() body: CreateReportDto) {
+    return this.reportService.createReport(body);
+  }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from 'src/reports/report.entity';
+import { CreateReportDto } from 'src/reports/dtos/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  async createReport(props: CreateReportDto) {
+    const report = this.repo.create(props);
+
+    return this.repo.save(report);
+  }
+}
